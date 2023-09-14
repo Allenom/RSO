8216d08056859a4471cab90074ac52fd65f7099d
frolovagit <a.a.frolova@hotmail.com>
2023-09-14 17:33:46 +0300

    Футер, Куки - загружена корректная вёрстка, адаптив

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -8,7 +8,7 @@ function getCookie(name) {
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
-let cookiecook = getCookie("cookiecook"),
+let cookiecook = getCookie("cookie"),
 cookiewin = document.getElementsByClassName('cookie')[0];
 // проверяем, есть ли у нас cookie, с которой мы не показываем окно и если нет, запускаем показ
 if (cookiecook != "no") {
