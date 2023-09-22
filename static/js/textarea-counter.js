4ce5a428c75fc0d34eb74c9428333dc6432bc592
Kristina Tarantsova <124876009+KristinaTarantsova@users.noreply.github.com>
2023-09-22 20:52:45 +0300

    верификация

--- static/js/textarea-counter.js
+++ static/js/textarea-counter.js
@@ -1,5 +1,5 @@
 // Счетчик символов в воде текстариа
-const textarea = document.querySelector('textarea');
+/*const textarea = document.querySelector('textarea');
 const counter = document.querySelector('[name=counter]');
 const maxlength = 400;
 
@@ -9,4 +9,13 @@ function onInput(event) {
   event.target.value = event.target.value.split(' ').join('').substr(0, maxlength); // обрезаем текст до 360 символов
   const length = event.target.value.length;
   counter.textContent = length;
-}
+}*/
+
+//const textarea = document.querySelector('textarea');
+//const counter = document.querySelector(.total);
+//const maxlength = 400;
+//
+//function countLetters(){
+//const text = textarea.value;
+//const textLength = textarea.value.length;
+//count.innerText = `${textLength}`;
