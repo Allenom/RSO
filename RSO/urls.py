7002163c74089c3cf365a669a7eaa0282f19d7d5
Danil <danil.ignatkin@gmail.com>
2023-10-02 22:01:11 +0300

    Update urls.py

--- RSO/urls.py
+++ RSO/urls.py
@@ -51,9 +51,9 @@ urlpatterns = [
     path("privacy_policy/", page, {'template': 'privacy_policy.html'}, name='privacy_policy'),
     path("terms_of_use/", page, {'template': 'terms_of_use.html'}, name='terms_of_use'),
 
-    #Отряды
+    # Отряды
 
-    path("squads/squads/", page,{'template': 'squads.html'}, name='squads'),
+    path("squads/squads/", page, {'template': 'squads/squads.html'}, name='squads'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
