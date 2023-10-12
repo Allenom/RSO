86463b6b9c3f2abea80d98503143661f646fbcb2
Kristina Tarantsova <124876009+KristinaTarantsova@users.noreply.github.com>
2023-10-12 19:48:14 +0300

    participants-lso

---
+++ static/js/participants_lso/participants_lso-approvals.js
@@ -0,0 +1,437 @@
+const squadslso = [
+  {
+
+    title: "Андрей",
+    category: "Уже в отряде",
+    desc: "Командир",
+    full: "Васильев Андрей Владимирович",
+    image: "/static/images/img/participants_lso_img/foto-user-1.png",
+    peoples: 12,
+    createdAt: "13.07.2000",
+    post:"",
+  },
+  {
+    title: "Мария",
+    category: "Уже в отряде",
+    desc: "Комиссар",
+    full: "Антонова Мария Владимировна",
+    image: "/static/images/img/participants_lso_img/foto-user-2.png",
+    peoples: 19,
+    createdAt: "13.07.2000",
+    post:"",
+  },
+  {
+    title: "Екатерина",
+    category: "Уже в отряде",
+    desc: "Мастер",
+    full: "Антонова Екатерина Владимировна",
+    image: "/static/images/img/participants_lso_img/foto-user-3.png",
+    peoples: 5,
+    createdAt: "13.07.2000",
+    post:"",
+  },
+  {
+    title: "Иван",
+    category: "Уже в отряде",
+    desc: "Медик",
+    full: "Гончаров Иван Олегович",
+    image: "/static/images/img/participants_lso_img/artem.jpg",
+    peoples: 8,
+    createdAt: "13.07.2000",
+    post:"",
+  },
+  {
+    title: "Елена",
+    category: "Уже в отряде",
+    desc: "Флагоносец",
+    full: "Ермолова Елена Андреевна",
+    image: "/static/images/img/participants_lso_img/foto-user-5.png",
+    peoples: 7,
+    createdAt: "13.07.2000",
+    education: "Амурская государственная медицинская академия"
+  },
+  {
+    title: "Егор",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Шашков Егор Андреевич",
+    image: "/static/images/img/participants_lso_img/foto-user-6.png",
+    peoples: 14,
+    createdAt: "13.07.2000",
+    education: "Амурский государственный университет"
+  },
+  {
+    title: "Алексей",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Федоров Алексей Викторович",
+    image: "/static/images/img/participants_lso_img/foto-user-7.png",
+    peoples: 12,
+    createdAt: "13.07.2000",
+    education: "МГУ"
+  },
+  {
+    title: "Максим",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Хромых Максим Олегович",
+    image: "/static/images/img/participants_lso_img/foto-user-8.png",
+    peoples: 22,
+    createdAt: "13.07.2000",
+    education: "Университет имени Баумана"
+  },
+  {
+    title: "Евгения",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Фролова Евгения Васильевна",
+    image: "/static/images/img/participants_lso_img/foto-user-9.png",
+    peoples: 12,
+    createdAt: "13.07.2000",
+    education: "Амурская государственная медицинская академия"
+  },
+  {
+    title: "Елена",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Кошкина Елена Максимовна",
+    image: "/static/images/img/participants_lso_img/foto-user-10.png",
+    peoples: 2,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Роланда",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Ким Ролада Мехайловна",
+    image: "/static/images/img/participants_lso_img/foto-user-11.png",
+    peoples: 5,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Екатерина",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Шило Екатерина Андреевна",
+    image: "/static/images/img/participants_lso_img/foto-user-12.png",
+    peoples: 14,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Вероника",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Таран Вероника Владиславовна",
+    image: "/static/images/img/participants_lso_img/foto-user-13.png",
+    peoples: 22,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Ирина",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Линейные студенческие отряды",
+    image: "/static/images/img/participants_lso_img/foto-user-14.png",
+    peoples: 10,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Ярослава",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Китова Ярослава Олеговна",
+    image: "/static/images/img/participants_lso_img/foto-user-15.png",
+    peoples: 4,
+    createdAt: "13.07.2000",
+
+  },
+  {
+    title: "Анна",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Лопатина Анна Дмитривна",
+    image: "/static/images/img/participants_lso_img/foto-user-16.png",
+    peoples: 6,
+    createdAt: "13.07.2000",
+
+  },
+  {
+    title: "Василиса",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Принц Василиса Эдуардовна",
+    image: "/static/images/img/participants_lso_img/foto-user-17.png",
+    peoples: 25,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Анна",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Митерева Анна Анатольевна",
+    image: "/static/images/img/participants_lso_img/foto-user-18.png",
+    peoples: 11,
+    createdAt: "13.07.2000",
+  },
+  {
+    title: "Анна",
+    category: "Уже в отряде",
+    desc: "Боец",
+    full: "Дмитренко Анна Егоровна",
+    image: "/static/images/img/participants_lso_img/foto-user-19.png",
+    peoples: 17,
+    createdAt: "13.07.2000",
+
+  },
+
+  {
+    title: "Дмитрий",
+    category: "Ожидает одобрения",
+    desc: "Боец",
+    full: "Петров Дмитрий Артемович",
+    image: "/static/images/img/participants_lso_img/foto-user-22.png",
+    peoples: 12,
+    createdAt: "13.07.2000",
+
+  },
+  {
+    title: "Анна",
+    category: "Ожидает одобрения",
+    desc: "Боец",
+    full: "Ганькина Анна Георгиевна",
+    image: "/static/images/img/participants_lso_img/foto-user-21.png",
+    peoples: 10,
+    createdAt: "13.07.2000",
+
+  },
+  {
+    title: "Павел",
+    category: "Ожидает одобрения",
+    desc: "Боец",
+    full: "Шнур Павел Васильевич",
+    image: "/static/images/img/participants_lso_img/foto-user-22.png",
+    peoples: 12,
+    createdAt: "13.07.2000",
+
+  },
+  {
+    title: "Денис",
+    category: "Ожидает одобрения",
+    desc: "Боец",
+    full: "Игнатов Денис Анатольевич",
+    image: "/static/images/img/participants_lso_img/foto-user-22.png",
+    peoples: 29,
+    createdAt: "13.07.2000",
+
+  },
+  {
+    title: "Дмитрий",
+    category: "Ожидает одобрения",
+    desc: "Боец",
+    full: "Петров Дмитрий Артемович",
+    image: "/static/images/img/participants_lso_img/foto-user-22.png",
+    peoples: 25,
+    createdAt: "13.07.2000",
+  },
+];
+
+const filtered = [];
+
+
+// поиск
+const productsContainer = document.querySelector("#squadslso-wrapper");
+const categoryList = document.querySelector(".squadslso-tabs");
+
+const productsContainerHorizontal = document.querySelector("#horizontallso");
+
+function displayProducts(products) {
+  if (products.length > 0) {
+    const product_details = products
+      .map(
+        (product) => `
+  <div class="squadslso-wrapper__item">
+  <a class="squadslso__avatar-hover-a" href="#">Перейти в профиль  </a>
+    <div class="round-img">
+      <img src="${product.image}" alt="${product.title}" />
+    </div>
+    <div class="container">
+      <p class="squadslso-wrapper__item-category">${product.title}</p>
+      <p class="squadslso-wrapper__item-title">${product.desc}</p>
+    </div>
+  
+  </div>`
+      )
+      .join("");
+
+    productsContainer.innerHTML = product_details;
+  } else {
+    productsContainer.innerHTML = "<h4>Участник не найден</h4>";
+  }
+}
+// горизонтальное
+
+function displayHorizontal(products) {
+  if (products.length > 0) {
+    const product_horizontal = products
+      .map(
+        (product) => `
+  <div class="horizontallso-item">
+  <div class="horizontallso-item__wrapper">
+  <div class="horizontallso-img">
+    <img src="${product.image}" alt="${product.title}" />
+  </div>
+  <div class="containerHorizontal"
+  <div class="squads-wrapper__item-wrapper"
+  <p class="squads-wrapper__item-category-full">${product.full}</p>
+  </div>
+  <div class="participants-lso__list-date"style="border-left:2px solid #B6B6B6; padding-right: 8px;" >
+  <p>${product.createdAt}</p>
+</div>
+</div>
+</div>
+  </div>`
+      )
+      .join("");
+
+      productsContainerHorizontal.innerHTML = product_horizontal;
+  }
+
+  else {
+    productsContainerHorizontal.innerHTML = "<h4>Участник не найден</h4>";
+  }
+
+}
+function allSquads(squads) {
+  displayProducts(squads);
+  displayHorizontal(squads);
+}
+
+// 0000000000000000000000000000000000000000
+
+function setCategories() {
+  const allCategories = squadslso.map((product) => product.category);
+  //console.log(allCategories);
+  const catagories = [
+    ...allCategories.filter((product, index) => {
+      return allCategories.indexOf(product) === index;
+    }),
+  ];
+  //console.log(catagories);
+  categoryList.innerHTML = catagories
+  .map((category) => `<li class="squadslso-tabs__item">${category}</li>`)
+  .join("");
+
+  categoryList.addEventListener("click", (e) => {
+    let selectedCatagory = e.target.textContent;
+    if( selectedCatagory === "Уже в отряде") {
+      allSquads(squadslso)
+    } else {
+      allSquads(
+        squadslso.filter((product) => product.category == selectedCatagory)
+      );
+    }
+
+  });
+  // categoryList.addEventListener("click", (e) => {
+  //   const selectedCatagory = e.target.textContent;
+  //   selectedCatagory === "Все" ? displayProducts(squadslso) : displayProducts(squadslso.filter((product) => product.category == selectedCatagory));
+  // });
+}
+
+
+
+// 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+
+
+function changeLayout(val) {
+  if (val == 1) {
+    document.getElementById("squadslso-wrapper").style.display = "grid";
+    document.getElementById("horizontallso").style.display = "none";
+    document.querySelector(".menu-btn-horisontallso").classList.remove('active-change__menu');
+    document.querySelector(".menu-btn-block").classList.remove('active-change');
+  } else if (val == 2) {
+    document.getElementById("squadslso-wrapper").style.display = "none";
+    document.getElementById("horizontallso").style.display = "grid";
+    document.querySelector(".menu-btn-horisontallso").classList.add('active-change__menu');
+    document.querySelector(".menu-btn-block").classList.add('active-change');
+  }
+}
+
+const sortByDropdown = document.querySelector(".sort-alph");
+const sortOrderDropdown = document.querySelector(".sort-order");
+const EducationList = document.querySelector(".education-check");
+
+
+sortByDropdown.addEventListener("change", () => {
+  const sortByValue = sortByDropdown.value; // price or ram value
+  const sortOrderValue = sortOrderDropdown.value; // asc or desc value
+
+  const sorted = _.orderBy(squadslso, [sortByValue], sortOrderValue);
+
+  displayProducts(sorted);
+});
+
+sortOrderDropdown.addEventListener("change", () => {
+  const event = new Event("change");
+  const sortByValue = sortByDropdown.value;
+
+  if (sortByValue) {
+    sortByDropdown.dispatchEvent(event);
+  }
+});
+
+
+const txtSearch = document.querySelector("#search");
+txtSearch.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase().trim();
+  if (value) {
+    displayProducts(squadslso.filter((product) => product.title.toLowerCase().indexOf(value) !== -1));
+  } else {
+    displayProducts(squadslso);
+  }
+});
+
+displayProducts(squadslso);
+setCategories();
+
+sortByDropdown.addEventListener("change", () => {
+  const sortByValue = sortByDropdown.value; // price or ram value
+  const sortOrderValue = sortOrderDropdown.value; // asc or desc value
+  // const sortByEduc = EducationList.value
+
+  const sorted = _.orderBy(squadslso, [sortByValue], sortOrderValue);
+
+ allSquads(sorted);
+});
+
+sortOrderDropdown.addEventListener("change", () => {
+  const event = new Event("change");
+  const sortByValue = sortByDropdown.value;
+  // const sortByEduc= sortByDropdown.value;
+
+  if (sortByValue) {
+    sortByDropdown.dispatchEvent(event);
+  }
+});
+
+
+const txtSearchlso = document.querySelector("#search");
+txtSearch.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase().trim();
+  if (value) {
+    allSquads(
+      squadslso.filter(
+        (product) => product.title.toLowerCase().indexOf(value) !== -1
+      )
+    );
+  } else {
+    allSquads(squadslso);
+  }
+});
+
+allSquads(squadslso);
+setCategories();
+setEducations();
+
+// 000000000000000000000000000000000000
